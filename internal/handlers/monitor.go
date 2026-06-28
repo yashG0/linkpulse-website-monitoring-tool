8e400d7ef5dc64e97def0b6d363bb3fac9493158
yashG0 <yashgaurkar0@gmail.com>
2026-06-28 13:11:26 +0530

    chore: create monitor handler

--- internal/handlers/monitor.go
+++ internal/handlers/monitor.go
@@ -1,23 +1,140 @@
 package handlers
 
-import "net/http"
+import (
+	"database/sql"
+	"encoding/json"
+	"net/http"
+	"strconv"
+
+	"github.com/yashg0/linkpulse-website-monitoring-tool/internal/db"
+	"github.com/yashg0/linkpulse-website-monitoring-tool/internal/models"
+)
 
 func getMonitor(w http.ResponseWriter, r *http.Request) {
-	
+	const prefix = "/api/monitors/"
+	idStr := r.URL.Path[len(prefix):]
+	idInt, err := strconv.Atoi(idStr)
+
+	if err != nil {
+		http.Error(w, "Invalid monitor ID", http.StatusBadRequest)
+		return
+	}
+
+	monitor, err := db.GetMonitor(idInt)
+	if err == sql.ErrNoRows {
+		http.Error(w, "Monitor not found", http.StatusNotFound)
+		return
+	}
+
+	if err != nil {
+		http.Error(w, "Failed to fetched monitor", http.StatusInternalServerError)
+		return
+	}
+
+	w.Header().Set("Content-Type", "application/json")
+	w.WriteHeader(http.StatusOK)
+	json.NewEncoder(w).Encode(monitor)
 }
 
 func createMonitor(w http.ResponseWriter, r *http.Request) {
-	
+	defer r.Body.Close()
+
+	var m models.Monitor
+	deco := json.NewDecoder(r.Body)
+	deco.DisallowUnknownFields()
+	err := deco.Decode(&m)
+
+	if err != nil {
+		http.Error(w, "Failed to read value", http.StatusBadRequest)
+		return
+	}
+	if m.Name == "" || m.URL == "" {
+		http.Error(w, "Missing required fields", http.StatusBadRequest)
+		return
+	}
+	err = db.CreateMonitor(&m)
+	if err != nil {
+		http.Error(w, "Failed to create monitor", http.StatusInternalServerError)
+		return
+	}
+
+	w.Header().Set("Content-Type", "application/json")
+	w.WriteHeader(http.StatusCreated)
+	json.NewEncoder(w).Encode(m)
 }
 
 func getAllMonitors(w http.ResponseWriter, r *http.Request) {
+	monitors, err := db.GetAllMonitors()
+	if err != nil {
+		http.Error(w, "Failed to fetched data", http.StatusInternalServerError)
+		return
+	}
 
+	w.Header().Set("Content-Type", "application/json")
+	w.WriteHeader(http.StatusOK)
+	json.NewEncoder(w).Encode(monitors)
 }
 
 func updateMonitor(w http.ResponseWriter, r *http.Request) {
+	defer r.Body.Close()
 
+	const prefix = "/api/monitors/"
+	idStr := r.URL.Path[len(prefix):]
+	idInt, err := strconv.Atoi(idStr)
+	if err != nil {
+		http.Error(w, "Invalid monitor ID", http.StatusBadRequest)
+		return
+	}
+
+	var newMonitor models.Monitor
+
+	deco := json.NewDecoder(r.Body)
+	deco.DisallowUnknownFields()
+	err = deco.Decode(&newMonitor)
+
+	if err != nil {
+		http.Error(w, "Failed to read value", http.StatusBadRequest)
+		return
+	}
+
+	if newMonitor.Name == "" || newMonitor.URL == "" {
+		http.Error(w, "Missing required fields", http.StatusBadRequest)
+		return
+	}
+
+	err = db.UpdateMonitor(idInt, newMonitor)
+	if err == sql.ErrNoRows {
+		http.Error(w, "Monitor not found", http.StatusNotFound)
+		return
+	}
+
+	if err != nil {
+		http.Error(w, "Database error", http.StatusInternalServerError)
+		return
+	}
+	w.WriteHeader(http.StatusNoContent)
 }
+
 func deleteMonitor(w http.ResponseWriter, r *http.Request) {
+	const prefix = "/api/monitors/"
+	idStr := r.URL.Path[len(prefix):]
+	idInt, err := strconv.Atoi(idStr)
+	if err != nil {
+		http.Error(w, "Invalid monitor ID", http.StatusBadRequest)
+		return
+	}
+
+	err = db.DeleteMonitor(idInt)
+	if err == sql.ErrNoRows {
+		http.Error(w, "Monitor not found", http.StatusNotFound)
+		return
+	}
+
+	if err != nil {
+		http.Error(w, "Database error", http.StatusInternalServerError)
+		return
+	}
+	w.WriteHeader(http.StatusNoContent)
 
 }
 
