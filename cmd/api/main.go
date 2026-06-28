4b83d94197f6b8db9a7d422d1444f90b87c19e9e
yashG0 <yashgaurkar0@gmail.com>
2026-06-28 09:27:41 +0530

    chore: run server

--- cmd/api/main.go
+++ cmd/api/main.go
@@ -1,5 +1,29 @@
 package main
 
-func main(){
-	
+import (
+	"fmt"
+	"log"
+	"net/http"
+	"os"
+
+	"github.com/joho/godotenv"
+)
+
+func main() {
+	err := godotenv.Load()
+	if err != nil {
+		log.Fatal("Failed to load .env")
+	}
+
+	http.HandleFunc("/api/test", func(w http.ResponseWriter, r *http.Request) {
+		fmt.Fprintln(w, "Working Done!")
+	})
+	port := os.Getenv("PORT")
+
+	if port == "" {
+		log.Fatal("PORT is not set")
+	}
+
+	log.Println("Server is running at", port)
+	log.Fatal(http.ListenAndServe(port, nil))
 }
--- go.mod
+++ go.mod
@@ -1,3 +1,5 @@
 module github.com/yashg0/linkpulse-website-monitoring-tool
 
 go 1.26.4
+
+require github.com/joho/godotenv v1.5.1 // indirect
