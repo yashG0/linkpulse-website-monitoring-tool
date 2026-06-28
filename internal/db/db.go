0c1607a6f54dd9c4f720e98e8a8e767d96820ac1
yashG0 <yashgaurkar0@gmail.com>
2026-06-28 10:37:41 +0530

    chore: create database table creation completed

--- internal/db/db.go
+++ internal/db/db.go
@@ -22,4 +22,44 @@ func Init(cfg config.Config) {
 		log.Fatalf("Failed to connect to database: %v", err)
 	}
 	DB = db
+
+	_, err = DB.Exec("PRAGMA foreign_keys = ON;")
+	if err != nil {
+		log.Fatalf("Failed to enable foreign keys: %v", err)
+	}
+
+	createTables()
+}
+
+func createTables() {
+	query := `
+		CREATE TABLE IF NOT EXISTS monitors (
+			id INTEGER PRIMARY KEY AUTOINCREMENT,
+			name TEXT NOT NULL,
+			url TEXT NOT NULL UNIQUE,
+			interval INTEGER NOT NULL,
+			enabled BOOLEAN NOT NULL
+		);`
+
+	_, err := DB.Exec(query)
+	if err != nil {
+		log.Fatalf("Failed to create monitors table: %v", err)
+	}
+
+	query = `
+		CREATE TABLE IF NOT EXISTS check_results (
+			id INTEGER PRIMARY KEY AUTOINCREMENT,
+			monitor_id INTEGER NOT NULL,
+			status_code INTEGER NOT NULL,
+			response_time INTEGER NOT NULL,
+			success BOOLEAN NOT NULL,
+			checked_at DATETIME NOT NULL,
+			error_message TEXT,
+
+			FOREIGN KEY (monitor_id) REFERENCES monitors(id) ON DELETE CASCADE
+		);`
+	_, err = DB.Exec(query)
+	if err != nil {
+		log.Fatalf("Failed to create check_results table: %v", err)
+	}
 }
