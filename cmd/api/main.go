a401e6fcf4639b9c5b928c5d17a7822270237b98
yashG0 <yashgaurkar0@gmail.com>
2026-06-29 17:56:55 +0530

    chore: write worker funtion

--- cmd/api/main.go
+++ cmd/api/main.go
@@ -1,6 +1,7 @@
 package main
 
 import (
+	"github.com/yashg0/linkpulse-website-monitoring-tool/internal/checker"
 	"github.com/yashg0/linkpulse-website-monitoring-tool/internal/config"
 	"github.com/yashg0/linkpulse-website-monitoring-tool/internal/db"
 	"github.com/yashg0/linkpulse-website-monitoring-tool/internal/server"
@@ -12,5 +13,6 @@ func main() {
 
 	db.Init(cfg)
 	defer db.DB.Close()
+	go checker.StartWorker()
 	server.Start(cfg)
 }
--- internal/checker/worker.go
+++ internal/checker/worker.go
@@ -1,10 +1,27 @@
 package checker
 
-import "github.com/yashg0/linkpulse-website-monitoring-tool/internal/checker"
+import (
+	"log"
+	"time"
 
+	"github.com/yashg0/linkpulse-website-monitoring-tool/internal/db"
+)
 
 func StartWorker() {
-	for{
-		
+	for {
+		monitors, err := db.GetEnabledMonitors()
+		if err != nil {
+			continue
+		}
+
+		for _, m := range monitors {
+			result := CheckMonitor(&m)
+			err := db.CreateResult(&result)
+			if err != nil {
+				log.Print(err)
+				continue
+			}
+		}
+		time.Sleep(1 * time.Minute)
 	}
 }
